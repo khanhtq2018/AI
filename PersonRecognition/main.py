99b5f3c0787c76a39bed5ceeef16897b83abd95f
Khanh Tran <khanhtq7@fsoft.com.vn>
2018-12-20 04:22:14 +0700

    modify html

--- PersonRecognition/main.py
+++ PersonRecognition/main.py
@@ -5,12 +5,22 @@ Created on Wed Dec 19 18:35:38 2018
 @author: KhanhTQ7
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/personRecognition")
 def main():
     return render_template('index.html')
-
 if __name__ == "__main__":
     app.run()
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    # read the posted values from the UI
+    test_image = request.form['img']
+ 
+    # validate the received values
+    if test_image:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
